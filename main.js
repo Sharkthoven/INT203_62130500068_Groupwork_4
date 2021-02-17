33b02f0c3651279d71d64b6e7b8557ec11aeed2b
Sharkthoven <phichitphol.liszt@mail.kmutt.ac.th>
2021-02-17 12:23:03 +0700

    [Add] Files for work 4 -u -f

---
+++ main.js
@@ -0,0 +1,60 @@
+const app = {
+    data() {
+        return {
+            msg: 'Photo Gallery',
+            photosets: [
+                        {url:'img/Beethoven.jpg', alt: "Ludwig V. Beethoven", done: false, show: true}, 
+                        {url:'img/Chopin.jpg', alt: "Frederic Chopin", done: false, show: true},
+                        {url:'img/Liszt.jpg', alt: "Franz Liszt", done: false, show: true}, 
+                        {url:'img/Mozart.jpg', alt: "Joannes A. Mozart", done: false, show: true}
+                       ],
+            inputTask: "",
+            searchIconBTN: true,
+            cancelFunc: false,
+            noPhoto: false,
+        }
+    },
+
+    methods: {
+        search() {
+            this.searchIconBTN = false;
+            this.cancelFunc = true;
+        },
+        cancel() {
+            this.searchIconBTN = true;
+            this.cancelFunc = false;
+            for (let i = 0; i < this.photosets.length; i++) {
+                    this.photosets[i].show = true;
+            }
+        },
+        searchingFunc() {
+
+            let falseCount = 0;
+
+            for (let i = 0; i < this.photosets.length; i++) {
+                //This loop will check photo that same with your text
+                if(this.photosets[i].alt.toLowerCase().includes(this.inputTask.toLowerCase())) {
+                    this.photosets[i].show = true;
+                }
+                else {
+                    this.photosets[i].show = false;
+                    falseCount ++;
+                }
+                //This loop will check if no photo match will show [No Photo]
+                if(falseCount == this.photosets.length) {
+                    this.noPhoto = true;
+                }
+            }
+        }
+    },
+
+    computed: {
+        numberOfPicture() {
+            return this.photosets.length;
+        },
+        numberOfLiked() {
+            return this.photosets.filter(p => p.done).length;
+        }
+    }
+}
+Vue.createApp(app).mount('#app')
